c55126a80d1ae4e1af988673a0b171741d11fd21
yashoza-neu <oza.y@northeastern.edu>
2020-02-19 16:21:23 -0500

    Chaning db.js and createDatabase.js

--- webapp/services/createSchema.js
+++ webapp/services/createSchema.js
@@ -2,22 +2,22 @@ const mysql = require('mysql');
 
 //mysql database connection
 var con = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD
+    host: "localhost",
+    user: "root",
+    password: "jerry1919"
 });
 
 con.connect(function (err) {
     if (err) throw err;
     else {
         console.log("Connected!");
-        con.query("DROP DATABASE IF EXISTS "+process.env.DATABASE+";", function (err, result) {
+        con.query("DROP DATABASE IF EXISTS userDB;", function (err, result) {
             if (err) throw err;
             else {
-                con.query("CREATE DATABASE "+process.env.DATABASE, function (err, result) {
+                con.query("CREATE DATABASE UserDB;", function (err, result) {
                     if (err) throw err;
                     else {
-                        var sql = `CREATE TABLE `+process.env.DATABASE+`.User(
+                        var sql = `CREATE TABLE UserDB.User(
             id varchar(36) NOT NULL,
             first_name varchar(45) NOT NULL,
             last_name varchar(45) NOT NULL,
@@ -31,7 +31,7 @@ con.connect(function (err) {
                         con.query(sql, function (err, result) {
                             if (err) throw err;
                             else {
-                                var sql1 = `CREATE TABLE `+process.env.DATABASE+`.Bill (
+                                var sql1 = `CREATE TABLE UserDB.Bill (
                                     id VARCHAR(36) NOT NULL COMMENT 'Creating Bill',
                                     created_ts DATETIME NOT NULL,
                                     updated_ts DATETIME NOT NULL,
@@ -44,15 +44,13 @@ con.connect(function (err) {
                                     categories JSON NOT NULL,
                                     attachment JSON NULL DEFAULT NULL,
                                     PRIMARY KEY (id),
-                                    INDEX fk_bill_owner_idx (owner_id ASC) VISIBLE,
-                                    CONSTRAINT fk_bill_owner
                                       FOREIGN KEY (owner_id)
-                                      REFERENCES`+process.env.DATABASE+ `.User (id))`;
+                                      REFERENCES UserDB.User (id));`
                                 con.query(sql1, function (err, result) {
                                     if (err) throw err;
                                     else {
                                         console.log('Bill Table created');
-                                        var sql2 = `CREATE TABLE `+process.env.DATABASE+`.File (
+                                        var sql2 = `CREATE TABLE UserDB.File (
                                             file_name VARCHAR(255) NOT NULL,
                                             id VARCHAR(255) NOT NULL,
                                             url VARCHAR(255) NOT NULL,
