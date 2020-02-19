4f5dfdd2d2a3555877d075abfa8575d9c4f084e2
yashoza-neu <oza.y@northeastern.edu>
2020-02-19 15:24:16 -0500

    Creating Schema

--- webapp/services/createSchema.js
+++ webapp/services/createSchema.js
@@ -31,30 +31,43 @@ con.connect(function (err) {
                         con.query(sql, function (err, result) {
                             if (err) throw err;
                             else {
-                                var sql1 = `CREATE TABLE `+process.env.DATABASE+`.Recipe (
-                id varchar(36) NOT NULL COMMENT 'Creating Recipe',
-                created_ts datetime NOT NULL,
-                updated_ts datetime NOT NULL,
-                author_id varchar(36) NOT NULL,
-                cook_time_in_min int(11) NOT NULL,
-                prep_time_in_min int(11) NOT NULL,
-                total_time_in_min int(11) NOT NULL,
-                title varchar(60) NOT NULL,
-                cusine varchar(45) NOT NULL,
-                servings int(11) NOT NULL,
-                ingredients json NOT NULL,
-                steps json NOT NULL,
-                nutrition_information json NOT NULL,
-                PRIMARY KEY (id),
-                KEY fk_recipe_author_idx (author_id),
-                CONSTRAINT fk_recipe_author FOREIGN KEY (author_id) REFERENCES `+process.env.DATABASE+`.User (id) ON DELETE NO ACTION ON UPDATE NO ACTION
-              ) ENGINE=InnoDB DEFAULT CHARSET=latin1;`;
-
+                                var sql1 = `CREATE TABLE `+process.env.DATABASE+`.Bill (
+                                    id VARCHAR(36) NOT NULL COMMENT 'Creating Bill',
+                                    created_ts DATETIME NOT NULL,
+                                    updated_ts DATETIME NOT NULL,
+                                    owner_id VARCHAR(36) NOT NULL,
+                                    vendor VARCHAR(60) NOT NULL,
+                                    bill_date DATE NOT NULL,
+                                    due_date DATE NOT NULL,
+                                    amount_due DOUBLE NOT NULL,
+                                    paymentStatus ENUM('paid', 'due', 'past_due', 'no_payment_required') NOT NULL,
+                                    categories JSON NOT NULL,
+                                    attachment JSON NULL DEFAULT NULL,
+                                    PRIMARY KEY (id),
+                                    INDEX fk_bill_owner_idx (owner_id ASC) VISIBLE,
+                                    CONSTRAINT fk_bill_owner
+                                      FOREIGN KEY (owner_id)
+                                      REFERENCES`+process.env.DATABASE+ `.User (id))`;
                                 con.query(sql1, function (err, result) {
                                     if (err) throw err;
                                     else {
-                                        console.log('Recipe Table created');
-                                        process.exit(0);
+                                        console.log('Bill Table created');
+                                        var sql2 = `CREATE TABLE `+process.env.DATABASE+`.File (
+                                            file_name VARCHAR(255) NOT NULL,
+                                            id VARCHAR(255) NOT NULL,
+                                            url VARCHAR(255) NOT NULL,
+                                            upload_date DATE NOT NULL,
+                                            metadata VARCHAR(255) NULL DEFAULT NULL,
+                                            PRIMARY KEY (id))
+                                            ENGINE = InnoDB
+                                            DEFAULT CHARACTER SET = latin1;`
+                                        con.query(sql2, function (err, result) {
+                                            if (err) throw err;
+                                            else {
+                                                console.log('File Table created');
+                                                process.exit(0);
+                                            }
+                                        });
                                     }
                                 });
 
