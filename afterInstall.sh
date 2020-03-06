ee8a82d74a427550992a0c0f46be3e77b4148307
yashoza-neu <oza.y@northeastern.edu>
2020-03-06 07:15:43 -0500

    Removing Node_modules6

--- afterInstall.sh
+++ afterInstall.sh
@@ -6,4 +6,5 @@ aws configure list
 ls
 #sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-config.json -s
 cd /home/ubuntu/webapp
-sudo npm install
+# sudo npm install
+ls -la
