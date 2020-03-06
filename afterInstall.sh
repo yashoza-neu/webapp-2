7dcd2ee3f92c0eaad6721964b1c2f500c82acb77
yashoza-neu <oza.y@northeastern.edu>
2020-03-06 02:37:42 -0500

    Adding zip to S3

--- afterInstall.sh
+++ afterInstall.sh
@@ -5,5 +5,5 @@ aws configure set default.region us-east-1
 aws configure list
 ls
 #sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-config.json -s
-#cd /home/centos/webapp
-#sudo npm install
+cd /home/ubuntu/webapp
+sudo npm install
