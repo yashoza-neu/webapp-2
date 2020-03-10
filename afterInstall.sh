a23ba86a90022b8324bd344fd3b0352416e5fec7
yashoza-neu <oza.y@northeastern.edu>
2020-03-10 16:15:11 -0400

    Changing afterInstall

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 pwd
-whoami
 aws configure set default.region us-east-1
 aws configure list
 ls
-#sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-config.json -s
 cd /home/ubuntu/webapp
-# sudo npm install
-ls -la
+sudo npm install
