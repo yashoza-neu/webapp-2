3b632d5111572a407749700e35078bc14292dd53
Yashoza19 <yoyashoza@gmail.com>
2020-03-17 17:37:08 -0400

    Updating CircleCI

--- afterInstall.sh
+++ afterInstall.sh
@@ -3,6 +3,7 @@ pwd
 ls
 cd /home/ubuntu/webapp
 printenv
-sudo npm install --unsafe-perm=true --allow-root
+sudo chmod  -R 777 /home/ubuntu/webapp
+sudo npm install
 cd services
 node createSchema.js
