d7b48306b9f4ce21886c11d4248dae21406610b6
Yashoza19 <yoyashoza@gmail.com>
2020-03-18 00:12:14 -0400

    Adding cloudwatch-config.json to afterInstall

--- afterInstall.sh
+++ afterInstall.sh
@@ -3,6 +3,7 @@ pwd
 ls
 cd /home/ubuntu/webapp
 printenv
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
 sudo chmod  -R 777 /home/ubuntu/webapp
 sudo npm install
 cd services
